731a51640a70bcc78f8893f1760e7162ba439925
yardbird304499 <cole.hardy@ndus.edu>
2023-10-19 15:55:01 -0500

    Added all the forms

--- Term Project/Home.cs
+++ Term Project/Home.cs
@@ -1,8 +1,8 @@
 namespace Term_Project
 {
-    public partial class Form1 : Form
+    public partial class Home : Form
     {
-        public Form1()
+        public Home()
         {
             InitializeComponent();
         }
